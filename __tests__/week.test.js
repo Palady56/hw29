80bd558454f9c6edb6bd7a17dcc9fc939e8e97bd
Palady56 <paladyultra@gmail.com>
2024-07-30 21:05:36 +0300

    add describe to tests

--- __tests__/week.test.js
+++ __tests__/week.test.js
@@ -1,25 +1,27 @@
 const weekFn = require("../week")
 
-test("Week 1 should return Понеділок", () => {
-  expect(weekFn(1)).toBe("Понеділок")
-})
+describe("Test function weekFn", () => {
+  test("Week 1 should return Понеділок", () => {
+    expect(weekFn(1)).toBe("Понеділок")
+  })
 
-test("Week 3 should return Середа", () => {
-  expect(weekFn(3)).toBe("Середа")
-})
+  test("Week 3 should return Середа", () => {
+    expect(weekFn(3)).toBe("Середа")
+  })
 
-test("Week 7 should return Неділя", () => {
-  expect(weekFn(7)).toBe("Неділя")
-})
+  test("Week 7 should return Неділя", () => {
+    expect(weekFn(7)).toBe("Неділя")
+  })
 
-test("Week 9 should return null", () => {
-  expect(weekFn(9)).toBe(null)
-})
+  test("Week 9 should return null", () => {
+    expect(weekFn(9)).toBe(null)
+  })
 
-test("Week 1.5 should return null", () => {
-  expect(weekFn(1.5)).toBe(null)
-})
+  test("Week 1.5 should return null", () => {
+    expect(weekFn(1.5)).toBe(null)
+  })
 
-test("Week 2 should return Вівторок", () => {
-  expect(weekFn(2)).toBe("Вівторок")
+  test("Week 2 should return Вівторок", () => {
+    expect(weekFn(2)).toBe("Вівторок")
+  })
 })
